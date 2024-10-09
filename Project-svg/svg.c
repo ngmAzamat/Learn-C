9e565336bae5437b487d004730b4170397fe4c96
Azamat Nigmatullin <ngmazamat@gmail.com>
2024-10-09 11:08:29 +0400

    commit

--- Project-svg/svg.c
+++ Project-svg/svg.c
@@ -5,12 +5,12 @@ int main(int argc, char** argv) {
     FILE *file = fopen("new_flags.svg", "w");
     if (file == NULL) {
         perror("Error opening file");
-        return 1;Project-
+        return 1;
     }
-    int a;
-    printf("введите цвет в формате: #номер");
+    char* a;
+    printf("enter color code like that: #000000");
     scanf("%d",&a);
-    fprintf(file, "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"100px\" width=\"200px\"><rect height=\"100\" width=\"200\" fill=\"#000000\"/></svg>\n");
+    fprintf(file, "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"100px\" width=\"200px\"><rect height=\"100\" width=\"200\" fill=\"%c\"/></svg>\n", a);
     fclose(file);
     return 0;
 }
